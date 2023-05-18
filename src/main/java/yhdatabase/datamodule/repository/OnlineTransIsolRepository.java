9e9494d8dfa3b3e6f262860f874d053a5adfb20f
kdozlo <eodud5730@naver.com>
2023-05-18 09:53:45 +0900

    OnlineTransIsolService - filterSQLResult 구현 완료

--- src/main/java/yhdatabase/datamodule/repository/OnlineTransIsolRepository.java
+++ src/main/java/yhdatabase/datamodule/repository/OnlineTransIsolRepository.java
@@ -1,10 +1,8 @@
 package yhdatabase.datamodule.repository;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
 import yhdatabase.datamodule.domain.ProgWorkFlowMng;
 
 import javax.sql.DataSource;
--- src/main/java/yhdatabase/datamodule/service/OnlineTransIsolService.java
+++ src/main/java/yhdatabase/datamodule/service/OnlineTransIsolService.java
@@ -6,10 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import yhdatabase.datamodule.domain.ProgWorkFlowMng;
 import yhdatabase.datamodule.repository.OnlineTransIsolRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -35,29 +32,69 @@ public class OnlineTransIsolService {
         return result;
     }
 
-    @Transactional
-    public List<Map<String, Object>> filterSQLResult(List<Map<String, Object>> sqlResult, Optional<ProgWorkFlowMng> progWorkFlowMng) {
+    public Set<Map<String, Object>> filterSQLResult(List<Map<String, Object>> sqlResult, Optional<ProgWorkFlowMng> progWorkFlowMng) {
 
-        List<Map<String, Object>> result = new ArrayList<>();
+        Set<Map<String, Object>> result = new HashSet<>();
+
+        Map<String, String[]> filterCondList = new HashMap<>();
 
         System.out.println("filterSQLResult");
 
         for (Map<String, Object> row : sqlResult) {
-            // this.flowAttr.getString("col") 배열 값 보기 가능 ex. {"col" : ["idx", "user_no", "yyyymmdd"]}"
-            for( String key : row.keySet() ){
-                Object value = row.get(key);
+            for( String key : filterCondList.keySet() ){
+                String cond = filterCondList.get(key)[0];
+                String condValue = filterCondList.get(key)[1];
 
-                //해당하는 col 찾기
-                if(key.equals("user_no")) {
-                    //해당하는 col의 조건식 찾기
-                    //조건에 만족하는 값 찾기
-                    if (Integer.parseInt(value.toString()) > 13) {
-                        result.add(row);
-                    }
+                switch (cond) {
+                    case ">":
+                        if (Integer.parseInt(row.get(key).toString()) > Integer.parseInt(condValue)) {
+                            result.add(row);
+                        }
+                        break;
+
+                    case "<" :
+                        if (Integer.parseInt(row.get(key).toString()) < Integer.parseInt(condValue)) {
+                            result.add(row);
+                        }
+
+                        break;
+
+                    case ">=" :
+                        if (Integer.parseInt(row.get(key).toString()) >= Integer.parseInt(condValue)) {
+                            result.add(row);
+                        }
+
+                        break;
+
+                    case "<=" :
+                        if (Integer.parseInt(row.get(key).toString()) <= Integer.parseInt(condValue)) {
+                            result.add(row);
+                        }
+
+                        break;
+
+                    case "=" :
+                        if (row.get(key).toString().equals(condValue)) {
+                            result.add(row);
+                        }
+                        break;
+
+                    case "LIKE":
+                        if (row.get(key).toString().contains(condValue)) {
+                            result.add(row);
+                        }
+                        break;
+
+                    case "NOT LIKE":
+                        if (!row.get(key).toString().contains(condValue)) {
+                            result.add(row);
+                        }
+                        break;
                 }
             }
         }
 
+        //filterSQLResult 출력값
         for (Map<String, Object> row : result) {
             // this.flowAttr.getString("col") 배열 값 보기 가능 ex. {"col" : ["idx", "user_no", "yyyymmdd"]}"
             for( String key : row.keySet() ){
