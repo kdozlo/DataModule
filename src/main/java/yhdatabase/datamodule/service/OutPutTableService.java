7bfbb6857f7ea633d361f668124f2203d0ceeecb
kdozlo <eodud5730@naver.com>
2023-05-18 14:45:41 +0900

    OutPutTableService - createTable 구현

--- src/main/java/yhdatabase/datamodule/service/OutPutTableService.java
+++ src/main/java/yhdatabase/datamodule/service/OutPutTableService.java
@@ -2,8 +2,19 @@ package yhdatabase.datamodule.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import yhdatabase.datamodule.repository.OutPutTableRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class OutPutTableService {
+
+    private final OutPutTableRepository outPutTableRepository;
+
+    public Long createTable(Long progId, List<String> colInfo) {
+        outPutTableRepository.createTable(progId, colInfo);
+
+        return progId;
+    }
 }
